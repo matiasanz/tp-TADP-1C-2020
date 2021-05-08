774e6244aef20335fcdbf485cb3f28cd3e238572
matiasanz <matias.l.anzorandia@gmail.com>
2021-05-08 17:16:37 -0300

    Separo avances del objeto de prueba

---
+++ ruby/lib/ORM.rb
@@ -0,0 +1,32 @@
+class Class
+    def has_one(tipo, named)
+
+        if @atributosPersistibles.nil?
+            @atributosPersistibles = {}
+        end
+
+        @atributosPersistibles[named] = tipo
+    end
+
+    def atributos_persistibles()
+        @atributosPersistibles
+    end
+end
+
+class Object
+
+    def save!
+        self.class.atributos_persistibles.each do |nombre,tipo|
+            simboloAtributo = "@#{nombre}".to_sym
+            valor = instance_variable_get(simboloAtributo)
+
+            persistir(nombre,tipo,valor)
+        end
+    end
+
+    def persistir(named, tipo, valor)
+        puts "Persisti nombre: #{named}, tipo: #{tipo}, valor: #{valor.to_s.inspect}"
+    end
+
+end
+
