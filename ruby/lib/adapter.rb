80dc94df24b9ebc75fcef437a0d3e164088c7233
matiasanz <matias.l.anzorandia@gmail.com>
2021-05-09 23:37:24 -0300

    cambio nombre de un mensaje

--- ruby/lib/adapter.rb
+++ ruby/lib/adapter.rb
@@ -42,7 +42,7 @@ class Tabla
         datos.each { |key, value| objeto.instance_variable_set(key.to_param, value) }
     end
 
-    def actualizar_de_db(objeto)
+    def recuperar_de_db(objeto)
         datos = @tablaTADB.entries().select{|e| e[:id]==objeto.id}.first
         asignar_datos(objeto, datos)
     end
@@ -55,8 +55,8 @@ class Tabla
 
     def update(objeto, fila)
         id = objeto.id
-        @tablaTADB.delete(id)
         fila[:id] = id
+        @tablaTADB.delete(id)
         @tablaTADB.insert(fila)
     end
 end
