f912aea952eee9a2caefcf6e270c98b1ad1fe704
AlejandroDeheza <ale.deheza@gmail.com>
2021-05-28 15:10:54 -0300

    agrego all_instances

--- ruby/lib/AdministradorDeTabla.rb
+++ ruby/lib/AdministradorDeTabla.rb
@@ -1,4 +1,19 @@
 
 module AdministradorDeTabla
 
+  # redefino "all_instances" con respecto a EntidadPersistible para cortar con la recursion
+  def all_instances
+    if @tabla
+      all_instances_de_hijos + @tabla.entries.map { |entrada| generar_instancia(entrada) }
+    else
+      []
+    end
+  end
+
+  def generar_instancia(entrada_de_tabla)
+    instancia = self.new
+    instancia.send(:id=, entrada_de_tabla[:id])
+    instancia.refresh!
+  end
+
 end
