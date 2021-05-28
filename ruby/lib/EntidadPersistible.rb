4aaea7138f82c08970d93f3a1f9eae1f27632379
AlejandroDeheza <ale.deheza@gmail.com>
2021-05-28 15:22:02 -0300

    agrego find by

--- ruby/lib/EntidadPersistible.rb
+++ ruby/lib/EntidadPersistible.rb
@@ -16,4 +16,25 @@ module EntidadPersistible
     array_aux
   end
 
+  def respond_to_missing?(mensaje, priv = false)
+    instancia = self.new
+    if mensaje.to_s.start_with?("find_by_") && instancia.respond_to?(sin_find_by_(mensaje), false)
+      metodo = instancia.method(sin_find_by_(mensaje))
+      metodo.arity == 0 || super
+    else
+      super
+    end
+  end
+  
+  # TODO arreglar "sin_find_by_(mensaje)"
+
+  #naturalmente falla si el metodo tiene aridad != 0, porque asi esta definido en "respond_to_missing?"
+  def method_missing(mensaje, *args, &bloque)
+    if respond_to?(mensaje, false)
+      all_instances.select { |instancia| instancia.send(sin_find_by_(mensaje)) == args[0] }
+    else
+      super
+    end
+  end
+
 end
