6a502643622f35b03ae4ba6644aeab6715686185
AlejandroDeheza <ale.deheza@gmail.com>
2021-05-28 19:11:55 -0300

    extraigo metodos de ORM

--- ruby/lib/ORM.rb
+++ ruby/lib/ORM.rb
@@ -9,6 +9,12 @@ module ORM
   end
 
   def self.entregar_dependecias(modulo)
+    dependencias_modulos_y_clases(modulo)
+    dependencias_de_clases(modulo) if modulo.is_a?(Class)
+  end
+
+  private
+  def self.dependencias_modulos_y_clases(modulo)
     modulo.extend(EntidadPersistible)
     modulo.module_eval do
       def self.included(modulo)
@@ -16,21 +22,22 @@ module ORM
         modulos_hijos.push(modulo)
       end
     end
-    if modulo.is_a?(Class)
-      modulo.include(InstanciaPersistible)
-      modulo.extend(AdministradorDeTabla)
-      modulo.class_eval do
-        # esto inicializa los atributos que usan has_many con un array vacio []. Tambien inicializa los defaults
-        # si el usuario define un contructor, solo tiene que escribir "inicializar_atributos" (si lo usa)
-        # si no define contructor, funciona TOD0 bien
-        def initialize
-          inicializar_atributos
-          super
-        end
+  end
 
-        def self.inherited(clase)
-          modulos_hijos.push(clase)
-        end
+  def self.dependencias_de_clases(clase)
+    modulo.include(InstanciaPersistible)
+    modulo.extend(AdministradorDeTabla)
+    modulo.class_eval do
+      # esto inicializa los atributos que usan has_many con un array vacio []. Tambien inicializa los defaults
+      # si el usuario define un contructor, solo tiene que escribir "inicializar_atributos" (si lo usa)
+      # si no define contructor, funciona TOD0 bien
+      def initialize
+        inicializar_atributos
+        super
+      end
+
+      def self.inherited(clase)
+        modulos_hijos.push(clase)
       end
     end
   end
