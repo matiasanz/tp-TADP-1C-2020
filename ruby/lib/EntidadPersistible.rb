a39c8f6b3e99479625544633b4537af043c38970
AlejandroDeheza <ale.deheza@gmail.com>
2021-05-28 15:14:55 -0300

    agrego all_instances para modulos

--- ruby/lib/EntidadPersistible.rb
+++ ruby/lib/EntidadPersistible.rb
@@ -5,4 +5,15 @@ module EntidadPersistible
     @modulos_hijos ||= []
   end
 
+  # en AdministradorDeTabla redefino este metodo
+  def all_instances
+    all_instances_de_hijos
+  end
+
+  def all_instances_de_hijos
+    array_aux = []
+    modulos_hijos.each { |modulo| array_aux = array_aux + modulo.all_instances }
+    array_aux
+  end
+
 end
