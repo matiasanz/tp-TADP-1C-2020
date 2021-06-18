250a0678435a054ae8616e98a5647c37568f4ab5
matiasanz <matias.l.anzorandia@gmail.com>
2021-06-18 15:41:05 -0300

    0 no tiene color

--- scala/src/main/scala/Juego/Utils.scala
+++ scala/src/main/scala/Juego/Utils.scala
@@ -1,6 +1,4 @@
 package object Utils {
   type Plata = Double
   trait Resultado
-
-  def esPar(numero: Int) = numero%2 == 0
 }
--- scala/src/main/scala/Ruleta/Colores.scala
+++ scala/src/main/scala/Ruleta/Colores.scala
@@ -1,30 +1,32 @@
-import Utils.esPar
-
-import scala.::
-
 package object Colores {
 	trait Color
 
 	case object ROJO extends Color
 	case object NEGRO extends Color
+	case object INCOLORO extends Color
 
 	def color(numero: Int): Color = columna(numero) match{
+		case 0 => INCOLORO
 		case 1 => if(numero==10) NEGRO else color(numero+2)
 		case 2 => colorOpuesto(color(numero+1))
 		case 3 => if(numero%9==6) NEGRO else ROJO
 	}
 
 	def colorOpuesto(color: Color): Color = color match {
+		//TODO Incoloro no tiene opuesto
 		case NEGRO => ROJO
 		case ROJO => NEGRO
 	}
 
 	def columna(numero: Int): Int = {
 		if(numero<0 || numero >36){
-			throw new RuntimeException("numero no admitido: "+numero.toString)
+			throw new NumeroNoAdmitidoEnRuletaException(numero)
 		}
 
 		val columna = numero%3
-		if(columna==0) 3 else columna
+		if(columna==0 && numero!=0) 3 else columna
 	}
+
+	case class NumeroNoAdmitidoEnRuletaException(numero: Int)
+		extends RuntimeException("numero no admitido: "+numero.toString)
 }
